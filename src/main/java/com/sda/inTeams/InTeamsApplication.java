684afb96b880bbfc9b1156025bd9f0d1c9c7c440
BarCzerw <czerwonkabartlomiej@gmail.com>
2021-07-07 17:21:32 +0200

    Template Added spring boot template

---
+++ src/main/java/com/sda/inTeams/InTeamsApplication.java
@@ -0,0 +1,13 @@
+package com.sda.inTeams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InTeamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InTeamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sda/inTeams/InTeamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.inTeams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InTeamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
