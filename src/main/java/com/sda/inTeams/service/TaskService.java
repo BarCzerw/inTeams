939486eb088dda2c588695be56c374f10f734889
BarCzerw <czerwonkabartlomiej@gmail.com>
2021-07-14 13:11:50 +0200

    TaskService +adding and removing comments from task

--- src/main/java/com/sda/inTeams/service/TaskService.java
+++ src/main/java/com/sda/inTeams/service/TaskService.java
@@ -1,8 +1,10 @@
 package com.sda.inTeams.service;
 
 import com.sda.inTeams.exception.InvalidOperation;
+import com.sda.inTeams.model.Comment.Comment;
 import com.sda.inTeams.model.Task.Task;
 import com.sda.inTeams.model.Task.TaskStatus;
+import com.sda.inTeams.repository.CommentRepository;
 import com.sda.inTeams.repository.ProjectRepository;
 import com.sda.inTeams.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +20,7 @@ public class TaskService implements DatabaseManageable<Task> {
 
     private final TaskRepository taskRepository;
     private final ProjectRepository projectRepository;
+    private final CommentRepository commentRepository;
 
     public List<Task> getAll() {
         return taskRepository.findAll();
@@ -51,6 +54,26 @@ public class TaskService implements DatabaseManageable<Task> {
         taskToDelete.setProject(null);
         taskToDelete = saveToDatabase(taskToDelete);
         taskRepository.delete(taskToDelete);
+    }
+
+    public void addCommentToTask(long taskId, long commentId) throws InvalidOperation {
+        Task task = taskRepository.findById(taskId).orElseThrow(() -> new InvalidOperation("Task id:" + taskId + " not found!"));
+        Comment commentToAdd = commentRepository.findById(taskId).orElseThrow(() -> new InvalidOperation("Comment id:" + commentId + " not found!"));
+        task.getComments().add(commentToAdd);
+        saveToDatabase(task);
+        commentToAdd.setTask(task);
+        commentRepository.save(commentToAdd);
+    }
+
+    public void removeCommentFromTask(long taskId, long commentId) throws InvalidOperation {
+        Task task = taskRepository.findById(taskId).orElseThrow(() -> new InvalidOperation("Task id:" + taskId + " not found!"));
+        Comment commentToDelete = commentRepository.findById(taskId).orElseThrow(() -> new InvalidOperation("Comment id:" + commentId + " not found!"));
+        if (task.getComments().contains(commentToDelete)) {
+            commentRepository.delete(commentToDelete);
+            saveToDatabase(task);
+        } else {
+            throw new InvalidOperation("Cannot remove comment id:" + commentId + " from task id:" + taskId + " - Comment is not assigned to this Task!");
+        }
     }
 
     public void changeTaskStatus(long taskId, TaskStatus status) throws InvalidOperation {
