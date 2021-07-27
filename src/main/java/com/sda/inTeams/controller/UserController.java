d52c7a05e1c08ce3722b7e041fa88dce3f4dc079
BarCzerw <czerwonkabartlomiej@gmail.com>
2021-07-27 19:53:19 +0200

    User +fixed removing of user

--- src/main/java/com/sda/inTeams/controller/UserController.java
+++ src/main/java/com/sda/inTeams/controller/UserController.java
@@ -87,13 +87,12 @@ public class UserController {
     }
 
     @GetMapping("/delete/{id}")
-    public String deleteTeam(@PathVariable(name = "id") long userId) {
+    public String deleteUser(@PathVariable(name = "id") long userId) {
         try {
-            User userToDelete = userService.getByIdOrThrow(userId);
             userService.delete(userId);
         } catch (InvalidOperation invalidOperation) {
             invalidOperation.printStackTrace();
         }
-        return "redirect:/user/all";
+        return "redirect:/";
     }
 }
